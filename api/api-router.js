08de418251488d11799f118ff38f17689fea7a4e
frescocodes <hueydakotah@gmail.com>
2019-11-11 12:35:37 -0500

    hash password

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const Users = require('../users/users-model')
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -9,5 +11,22 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', ( req, res ) => {
+  // read password from body
+  // hash password 
+  // return it to the user in an object
+  let user = req.body
+  const hash = bcrypt.hashSync(user.password, 12)
+  user.password = hash;
+
+  Users.add(user)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+})
 
 module.exports = router;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
 
 const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
   let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12)
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
